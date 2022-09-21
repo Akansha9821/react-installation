2958873185fddda1ea880b45f8a449a3db1d4646
Akanksha Rana <akanksha.rana@neomediaworld.com>
2022-09-21 19:28:54 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <h1>Hii Akansha Rana, This is you're first app in react</h1>
 );
 
 // If you want to start measuring performance in your app, pass a function
